1e70302003832f30a44317e033fada79fdfabf68
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-11-24 19:51:11 -0300

    Commit inicial com esqueleto

---
+++ src/test/java/com/adsonsa/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsonsa.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
